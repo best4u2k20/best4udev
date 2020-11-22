0cb70f9727df2a08519d00609cfbc3c609a55531
MakeBigNews <qding1990@gmail.com>
2020-11-22 11:07:05 -0800

    add cart api

--- website_sale_api/controllers/main.py
+++ website_sale_api/controllers/main.py
@@ -7,7 +7,7 @@ from odoo.addons.website_sale.controllers.main import WebsiteSale
 from odoo.http import request
 from odoo.osv import expression
 from .serialization import serialize_order, serialize_products, serialize_categories
-
+from odoo.addons.payment.controllers.portal import PaymentProcessing
 
 class WebsiteSaleAPI(WebsiteSale):
 
@@ -95,7 +95,7 @@ class WebsiteSaleAPI(WebsiteSale):
         return values
 
     @http.route('/shop/api/product', type='json', auth='public', website=True)
-    def api_product(self, product_id=None, **post):
+    def product(self, product_id=None, **post):
         Product = request.env['product.template'].sudo()
         return serialize_products(Product.browse(product_id))
 
@@ -199,6 +199,52 @@ class WebsiteSaleAPI(WebsiteSale):
 
         return serialize_order(order)
 
+    @http.route(['/shop/api/payment/transaction/'], type='json', auth="public", website=True)
+    def api_payment_transaction(self, acquirer_id, **kwargs):
+        """ Json method that creates a payment.transaction, used to create a
+        transaction when the user clicks on 'pay now' button. After having
+        created the transaction, the event continues and the user is redirected
+        to the acquirer website.
+
+        :param int acquirer_id: id of a payment.acquirer record. If not set the
+                                user is redirected to the checkout page
+        """
+        # Ensure a payment acquirer is selected
+        if not acquirer_id:
+            return False
+
+        try:
+            acquirer_id = int(acquirer_id)
+        except:
+            return False
+
+        order = request.website.sale_get_order()
+
+        # Ensure there is something to proceed
+        if not order or (order and not order.order_line):
+            return False
+
+        assert order.partner_id.id != request.website.partner_id.id
+
+        # Create transaction
+        vals = {
+            'acquirer_id': acquirer_id,
+            'return_url': '/shop/payment/validate'
+        }
+
+        transaction = order._create_payment_transaction(vals)
+
+        # store the new transaction into the transaction list and if there's an old one, we remove it
+        # until the day the ecommerce supports multiple orders at the same time
+        last_tx_id = request.session.get('__website_sale_last_tx_id')
+        last_tx = request.env['payment.transaction'].browse(last_tx_id).sudo().exists()
+        if last_tx:
+            PaymentProcessing.remove_payment_transaction(last_tx)
+        PaymentProcessing.add_payment_transaction(transaction)
+        request.session['__website_sale_last_tx_id'] = transaction.id
+
+        return transaction.render_sale_api_values(order)
+
     @http.route(['/shop/api/countries'], type='json', methods=['POST'], auth="public", website=True)
     def api_countries(self):
         country = request.env['res.country'].search([])
@@ -208,3 +254,8 @@ class WebsiteSaleAPI(WebsiteSale):
     def api_states(self, country_id):
         country = request.env['res.country'].browse(country_id)
         return country.get_website_sale_states().read(fields=['id', 'name', 'code'])
+
+    @http.route(['/shop/api/acquirers'], type='json', methods=['POST'], auth="public", website=True)
+    def api_acquirers(self, ):
+        acquirers = request.env['payment.acquirer'].search([('state', '=', 'enabled')])
+        return acquirers.read(fields=['id', 'name', 'provider', 'state'])
